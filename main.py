a23e6a2e805a83033d4ee347d8b791ab3aef72a5
girvel <widauka@ya.ru>
2024-02-06 02:42:33 +0500

    Hello world!

---
+++ main.py
@@ -0,0 +1,10 @@
+import sdl2.ext
+
+sdl2.ext.init()
+window = sdl2.ext.Window("Hello world!", size=(640, 480))
+window.show()
+
+processor = sdl2.ext.TestEventProcessor()
+processor.run(window)
+
+sdl2.ext.quit()
